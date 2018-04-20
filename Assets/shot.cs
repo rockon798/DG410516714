d93ab210cc8c0f473786be0542c4d25193e6cb78
rockon798 <k0935731736@gmail.com>
2018-04-20 11:52:24 +0800

    第8題建2個ui text 寫程式在射擊物上面

--- Assets/shot.cs
+++ Assets/shot.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shot : MonoBehaviour {
 
+    public Text countText;
+    public Text winText;
+    private int count;
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
 	}
         public float speed;
+
         Rigidbody rigidbody; 
 	// Update is called once per frame
 	void Update () {
@@ -19,6 +27,16 @@ public class shot : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "勝利";
         }
     }
 }
