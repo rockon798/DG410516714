51365aead9a8e3f44b5ae502471cc24b111b8291
rockon798 <k0935731736@gmail.com>
2018-04-20 10:56:15 +0800

    第5題 用程式讓砲塔旋轉 按下->鍵向右旋轉 按下<-左鍵向左旋轉

---
+++ Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
